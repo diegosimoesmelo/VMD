6c0c6c5c5d580fcf386d8249cca7eaa3140798ab
diego.melo@datametrica.com.br-at-772485190534 <dasdmseomis@gmail.com>
2026-04-15 08:16:07 -0300

    correcao acentos

--- app/resources/views/vehicles/edit.blade.php
+++ app/resources/views/vehicles/edit.blade.php
@@ -1,10 +1,10 @@
-﻿@extends('layouts.panel', ['title' => 'Editar veÃ­culo'])
+﻿@extends('layouts.panel', ['title' => 'Editar veículo'])
 
 @section('content')
     @include('vehicles._vehicle_form', [
         'formAction' => route('vehicles.update', $vehicle),
         'formMethod' => 'PUT',
-        'submitLabel' => 'Atualizar veÃ­culo',
+        'submitLabel' => 'Atualizar veículo',
         'backUrl' => route('vehicles.index'),
         'vehicle' => $vehicle,
     ])
